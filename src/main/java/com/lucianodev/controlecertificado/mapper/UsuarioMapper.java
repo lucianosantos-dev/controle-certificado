496d246cad78820b0acf312fb46d24c6ca931ba3
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-06-16 21:38:29 -0300

    feat: implementa UsuarioMapper

---
+++ src/main/java/com/lucianodev/controlecertificado/mapper/UsuarioMapper.java
@@ -0,0 +1,30 @@
+package com.lucianodev.controlecertificado.mapper;
+
+import com.lucianodev.controlecertificado.dtos.request.UsuarioRequest;
+import com.lucianodev.controlecertificado.dtos.response.UsuarioResponse;
+import com.lucianodev.controlecertificado.entities.Usuario;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioMapper {
+
+    public Usuario toEntity(UsuarioRequest request) {
+        return Usuario.builder()
+                .nome(request.getNome())
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .senha(request.getSenha())
+                .perfil(request.getPerfil())
+                .build();
+    }
+
+    public UsuarioResponse toResponse(Usuario entity) {
+        return UsuarioResponse.builder()
+                .id(entity.getId())
+                .nome(entity.getNome())
+                .email(entity.getEmail())
+                .username(entity.getUsername())
+                .perfil(entity.getPerfil())
+                .build();
+    }
+}
