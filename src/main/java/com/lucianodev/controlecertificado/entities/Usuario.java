2c1aa6c0f4cc2a26ad511b8e9a006b29306fd9a6
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-06-22 20:51:05 -0300

    refactor: remove ROLE_

--- src/main/java/com/lucianodev/controlecertificado/entities/Usuario.java
+++ src/main/java/com/lucianodev/controlecertificado/entities/Usuario.java
@@ -43,7 +43,7 @@ public class Usuario implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + this.perfil.name()));
+        return List.of(new SimpleGrantedAuthority(this.perfil.name()));
     }
 
     @Override
