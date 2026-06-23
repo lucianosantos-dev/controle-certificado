ff1602e96f047eae5c38bae7fa95b1e1966c6458
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-06-23 16:47:59 -0300

    fix: libera cors para netlify

--- src/main/java/com/lucianodev/controlecertificado/config/SecurityConfig.java
+++ src/main/java/com/lucianodev/controlecertificado/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200",
+                "https://controle-certificado-inap.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
